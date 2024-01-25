f5594147a04d333b78150e67684c404f874a0679
Rajesh-Dehury <56267680+Rajesh-Dehury@users.noreply.github.com>
2024-01-25 13:52:34 +0530

    side nav

--- resources/views/layouts/side-nav.blade.php
+++ resources/views/layouts/side-nav.blade.php
@@ -29,18 +29,18 @@
                     </a> -->
                 </div>
                 <div class="mb-5 pb-5">
+                    <a href="{{route('express.change.password')}}" class="text-white mb-1 navLink d-block d-flex {{request()->routeIs('express.change.password')?'active':''}}">
+                        <span class="d-inline-block me-4">
+                            <i class="bi bi-gear"></i>
+                        </span>
+                        <span class="d-inline-block">Settings</span>
+                    </a>
                     <a href="{{route('express.logout')}}" class="text-white mb-3 navLink d-block d-flex">
                         <span class="d-inline-block me-4">
                             <i class="bi bi-power"></i>
                         </span>
                         <span class="d-inline-block">Logout</span>
                     </a>
-                    <!-- <a href="" class="text-white mb-1 navLink d-block d-flex">
-                        <span class="d-inline-block me-4">
-                            <i class="bi bi-gear"></i>
-                        </span>
-                        <span class="d-inline-block">Settings</span>
-                    </a> -->
                     <!-- <a href="" class="text-white mb-1 navLink d-block d-flex">
                         <span class="d-inline-block me-4">
                             <i class="bi bi-info-circle"></i>
